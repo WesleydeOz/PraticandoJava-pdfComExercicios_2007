18f50bba949496fb212a1110e2e4236dd74358f9
Wesley Mendonça <79870253+WesleydeOz@users.noreply.github.com>
2021-06-14 16:57:55 -0300

    Update and rename Exc5.java to Exc05.java

--- Exercicio_Apostila_Java_2007/Exc05.java
+++ Exercicio_Apostila_Java_2007/Exc05.java
@@ -2,17 +2,17 @@ package Exercicio_Apostila_Java_2007;
 
 import java.util.Scanner;
 
-public class Exc5 {
+public class Exc05 {
 
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 		
-		System.out.print("Digite um número: ");
+		System.out.print("Digite um nÃºmero: ");
 		int numero = scn.nextInt();
 		
 		int antecessor = numero - 1;
 		
-		System.out.println("O antecessor de " + numero + " é " + antecessor + ".");
+		System.out.println("O antecessor de " + numero + " Ã© " + antecessor + ".");
 		
 		scn.close();
 	}
