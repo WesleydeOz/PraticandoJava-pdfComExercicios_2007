fa4bf701dc00f26ea357fd6f618dd9e2cb1e55f0
Wesley Mendonça <79870253+WesleydeOz@users.noreply.github.com>
2021-06-14 16:58:11 -0300

    Update and rename Exc6.java to Exc06.java

--- Exercicio_Apostila_Java_2007/Exc06.java
+++ Exercicio_Apostila_Java_2007/Exc06.java
@@ -1,19 +1,19 @@
 package Exercicio_Apostila_Java_2007;
 import java.util.Scanner;
 
-public class Exc6 {
+public class Exc06 {
 
 	public static void main(String[] args) {
 		
 		Scanner scn = new Scanner(System.in);
 		
-		System.out.print("Digite a medida da altura do ret‚ngulo (cm): ");
+		System.out.print("Digite a medida da altura do ret√¢ngulo (cm): ");
 		int alt = scn.nextInt();
-		System.out.print("Digite a medida da base do tri‚ngulo (cm): ");
+		System.out.print("Digite a medida da base do tri√¢ngulo (cm): ");
 		int base = scn.nextInt();
 		
 		int area = base * alt;
-		System.out.println("A ·rea deste ret‚ngulo È de " + area + "cm≤");
+		System.out.println("A √°rea deste ret√¢ngulo √© de " + area + "cm¬≤");
 		
 		scn.close();
 		
