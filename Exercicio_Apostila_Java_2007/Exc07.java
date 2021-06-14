31c33298495b842080799f49d16392ec8e9fb764
Wesley Mendonça <79870253+WesleydeOz@users.noreply.github.com>
2021-06-14 16:57:41 -0300

    Update and rename Exc7.java to Exc07.java

--- Exercicio_Apostila_Java_2007/Exc07.java
+++ Exercicio_Apostila_Java_2007/Exc07.java
@@ -2,25 +2,25 @@ package Exercicio_Apostila_Java_2007;
 
 import java.util.Scanner;
 
-public class Exc7 {
+public class Exc07 {
 
 	public static void main(String[] args) {
 			Scanner scn = new Scanner(System.in);
 			
-			System.out.print("Informe quantos anos você possui: ");
+			System.out.print("Informe quantos anos vocÃª possui: ");
 			int anos = scn.nextInt();
 			
-			System.out.print("Quantos meses após seu último aniversário ?: ");
+			System.out.print("Quantos meses apÃ³s seu Ãºltimo aniversÃ¡rio ?: ");
 			int meses = scn.nextInt();
 			
-			System.out.print("Quantos dias após seu último mêsversário ?: ");
+			System.out.print("Quantos dias apÃ³s seu Ãºltimo mÃªsversÃ¡rio ?: ");
 			int dias = scn.nextInt();
 			
 			int anoDia = anos * 365;
 			int mesesDia = meses * 30;
 			
 			int resultado = anoDia + mesesDia + dias;
-			System.out.println("Sua idade, em dias, é " + resultado);
+			System.out.println("Sua idade, em dias, Ã© " + resultado);
 			
 			scn.close();
 
